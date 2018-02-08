6983b2f8a11d9c36c6b91a9081dda5dd0725f7c1
Miecznik Rafal <rafal.miecznik@nokia.com>
2018-02-08 15:59:58 +0100

    print template added

--- test_module.h
+++ test_module.h
@@ -8,7 +8,7 @@
 #ifndef TEST_MODULE_H_
 #define TEST_MODULE_H_
 
-#define AVR_SUPPORT
+//#define AVR_SUPPORT
 
 #include <stdint.h>
 #include <stdio.h>
@@ -40,34 +40,43 @@ struct UnitTest{
 };
 
 class Test{
-private:
-	template <typename t>
-	bool is_mem_equal(t& a, t& b, uint16_t size=0);
-	template <typename t>
-	bool is_mem_equal(t* a, t* b, uint16_t size=0);
-	void passed();
-	void failed();
-	UnitTest* unit_tests;
-	uint16_t unit_tests_cnt;
-	UnitTest* alloc_test();
-	static uint16_t test_count;
-	static uint16_t tests_passed;
-	static uint16_t tests_failed;
 public:
 	//what kind of data is compared
-	enum assert_type{
+	enum variable_type{
 		numeric,
 		character,
 		string,
 		array
 	};
 	template <typename t>
-	void assertEqual(const char* test_name, t& a, t& b, assert_type type=numeric, uint16_t size=0);
+	void assertEqual(const char* test_name, t* a, t* b, variable_type type=numeric, uint16_t size=0);
 	template <typename t>
-	void assertEqual(const char* test_name, t* a, t* b, assert_type type=numeric, uint16_t size=0);
+	void assertEqual(const char* test_name, t a, t b, variable_type type=numeric, uint16_t size=0);
+	template <typename t>
+	void assertNotEqual(const char* test_name, t* a, t* b, variable_type type=numeric, uint16_t size=0);
+	template <typename t>
+	void assertNotEqual(const char* test_name, t a, t b, variable_type type=numeric, uint16_t size=0);
 	void add_test(UnitTest& test);
 	Test();
 	 ~Test();
+private:
+	template <typename t>
+	bool is_mem_equal(t& a, t& b, uint16_t size=0);
+	template <typename t>
+	bool is_mem_equal(t* a, t* b, uint16_t size=0);
+	UnitTest* unit_tests;
+	uint16_t unit_tests_cnt;
+	UnitTest* alloc_test();
+	static uint16_t test_count;
+	static uint16_t tests_passed;
+	static uint16_t tests_failed;
+	//const strings
+	static const char passed_s[];
+	static const char failed_s[];
+	static const char assert_eq_cond_s[];
+	static const char assert_not_eq_cond_s[];
+	template <typename t>
+	void print_ut_result(t val_a, t val_b, char* assertion_type, char* assert_eq_cond, char* result, variable_type type, uint16_t size=0);
 };
 
 UnitTest* Test::alloc_test(){
@@ -82,58 +91,93 @@ void Test::add_test(UnitTest& test){
 }
 
 template <typename t>
-void Test:: assertEqual (const char* test_name, t& a, t& b, assert_type type, uint16_t size){
-	/*
-	 * template for numeric types
-	 */
+void Test:: assertEqual (const char* test_name, t* a, t* b, variable_type type, uint16_t size){
 	print_test_name(test_name);
+	char* result;
+	char* assert_eq_cond;
+	char assertion_type[] = "assert equal";
+	if(is_mem_equal(a, b, size)){
+		result=(char*)passed_s;
+		assert_eq_cond=(char*)assert_eq_cond_s;
+		tests_passed++;
+	}
+	else{
+		result=(char*)failed_s;
+		assert_eq_cond=(char*)assert_not_eq_cond_s;
+		tests_failed++;
+	}
 	if(size > 0 and type != array)
 		type = string;
-	switch (type) {
-		case numeric:
-			printf("assert equal %ld==%ld ", (long int)a, (long int)b);
-			break;
-		case character:
-			printf("assert equal %c==%c ", a, b);
-			break;
-		case string:
-			printf("assert equal %s==%s ", a, b);
-			break;
-		default:
-			break;
-	}
-	is_mem_equal(a, b, size) ? passed() : failed();
+	if(type != array)
+		print_ut_result(a, b, assertion_type, assert_eq_cond, result, type, size);
+	else
+		print_ut_result(a[0], b[0], assertion_type, assert_eq_cond, result, type, size);
 }
 
 template <typename t>
-void Test:: assertEqual (const char* test_name, t* a, t* b, assert_type type, uint16_t size){
+void Test:: assertEqual (const char* test_name, t a, t b, variable_type type, uint16_t size){
 	print_test_name(test_name);
+	char* result;
+	char* assert_eq_cond;
+	char assertion_type[] = "assert equal";
+	if(is_mem_equal(a, b, size)){
+		result=(char*)passed_s;
+		assert_eq_cond=(char*)assert_eq_cond_s;
+		tests_passed++;
+	}
+	else{
+		result=(char*)failed_s;
+		assert_eq_cond=(char*)assert_not_eq_cond_s;
+		tests_failed++;
+	}
 	if(size > 0 and type != array)
 		type = string;
-	switch (type) {
-		case numeric:
-			printf("assert equal %ld==%ld ", (long int)*a, (long int)*b);
-			break;
-		case character:
-			printf("assert equal %c==%c ", *a, *b);
-			break;
-		case string:
-			printf("assert equal %s==%s ", a, b);
-			break;
-		case array:
-			printf("assert equal ");
-			for(uint16_t i=0; i<size/sizeof(t); i++){
-				printf("%d, ", a[i]);
-			}
-			printf(" = ");
-			for(uint16_t i=0; i<size/sizeof(t); i++){
-				printf("%d, ", b[i]);
-			}
-			break;
-		default:
-			break;
+	print_ut_result(a, b, assertion_type, assert_eq_cond, result, type, size);
+}
+
+template <typename t>
+void Test:: assertNotEqual (const char* test_name, t* a, t* b, variable_type type, uint16_t size){
+	print_test_name(test_name);
+	char* result;
+	char* assert_eq_cond;
+	char assertion_type[] = "assert not equal";
+	if(not is_mem_equal(a, b, size)){
+		result=(char*)passed_s;
+		assert_eq_cond=(char*)assert_not_eq_cond_s;
+		tests_passed++;
 	}
-	is_mem_equal(a, b, size) ? passed() : failed();
+	else{
+		result=(char*)failed_s;
+		assert_eq_cond=(char*)assert_eq_cond_s;
+		tests_failed++;
+	}
+	if(size > 0 and type != array)
+		type = string;
+	if(type != array)
+		print_ut_result(a, b, assertion_type, assert_eq_cond, result, type, size);
+	else
+		print_ut_result(a[0], b[0], assertion_type, assert_eq_cond, result, type, size);
+}
+
+template <typename t>
+void Test:: assertNotEqual (const char* test_name, t a, t b, variable_type type, uint16_t size){
+	print_test_name(test_name);
+	char* result;
+	char* assert_eq_cond;
+	char assertion_type[] = "assert not equal";
+	if(not is_mem_equal(a, b, size)){
+		result=(char*)passed_s;
+		assert_eq_cond=(char*)assert_eq_cond_s;
+		tests_passed++;
+	}
+	else{
+		result=(char*)failed_s;
+		assert_eq_cond=(char*)assert_not_eq_cond_s;
+		tests_failed++;
+	}
+	if(size > 0 and type != array)
+		type = string;
+	print_ut_result(a, b, assertion_type, assert_eq_cond, result, type, size);
 }
 
 template <typename t>
@@ -143,11 +187,9 @@ bool Test::is_mem_equal(t& a, t& b, uint16_t size){
 	test_count++;
 	uint16_t memcmp_result = memcmp(&a, &b, size);
 	if(memcmp_result == 0){
-		tests_passed++;
 		return true;
 	}
 	else{
-		tests_failed++;
 		return false;
 	}
 }
@@ -159,13 +201,42 @@ bool Test::is_mem_equal(t* a, t* b, uint16_t size){
 	test_count++;
 	uint16_t memcmp_result = memcmp(a, b, size);
 	if(memcmp_result == 0){
-		tests_passed++;
 		return true;
 	}
 	else{
-		tests_failed++;
 		return false;
 	}
+}
+
+template <typename t>
+void Test::print_ut_result(t val_a, t val_b, char* assertion_type, char* assert_eq_cond, char* result, variable_type type, uint16_t size){
+	switch (type) {
+		case numeric:
+			printf("%s %ld %s %ld ", assertion_type, (long int)val_a, assert_eq_cond, (long int)val_b);
+			break;
+		case character:
+			printf("%s %c %s %c ", assertion_type, val_a, assert_eq_cond, val_b);
+			break;
+		case string:
+			printf("%s %s %s %s ", assertion_type, val_a, assert_eq_cond, val_b);
+			break;
+		case array:
+			{	t* pa = &val_a;
+				t* pb = &val_b;
+				printf("%s ", assertion_type);
+				for(uint16_t i=0; i<size/sizeof(t); i++){
+					printf("%d, ", pa[i]);
+				}
+				printf(" %s ", assert_eq_cond);
+				for(uint16_t i=0; i<size/sizeof(t); i++){
+					printf("%d, ", pb[i]);
+				}
+			}
+			break;
+		default:
+			break;
+	}
+	printf("%s\n", result);
 }
 
 Test::Test(){
@@ -188,13 +259,9 @@ Test::~Test(){
 uint16_t Test::test_count = 0;
 uint16_t Test::tests_passed = 0;
 uint16_t Test::tests_failed = 0;
-
-void Test::passed(){
-	printf("passed\n\n");
-}
-
-void Test::failed(){
-	printf("failed\n\n");
-}
+const char Test::passed_s[] = "passed";
+const char Test::failed_s[] = "failed";
+const char Test::assert_eq_cond_s[] = "==";
+const char Test::assert_not_eq_cond_s[] = "!=";
 
 #endif /* TEST_MODULE_H_ */
